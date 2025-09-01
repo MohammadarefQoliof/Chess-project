fab76d5777eb18f3ff1294e714ddd251d02d4ef7
Məhəmmədarif Quliyev <mhmmdarifquliyev33@gmail.com>
2025-09-01 18:39:53 +0400

    Chess project

--- JS/puzzles.js
+++ JS/puzzles.js
@@ -60,26 +60,165 @@ const today = new Date()
 const index = today.getDate() % puzzles.length;
 const puzzle = puzzles[index]
 
-let image = document.getElementById("image")
-let input = document.querySelector("#answer")
-let checkBtn = document.getElementById("checkBtn")
+let image = document.querySelector(".image")
+let input = document.querySelector(".answer")
+let checkBtn = document.querySelector(".checkBtn")
+let otherPuzzles = document.getElementById("otherPuzzles")
+let mateImage = document.querySelector(".mateImage")
+
 
 image.style.backgroundImage = `url(${puzzle.photo1})`
 let flag = true
 
+input.classList.remove("wrongAnswer")
+input.classList.remove("allRight")
+
 checkBtn.addEventListener("click", ()=>{
-    let answer = input.value.toLowerCase()
+    let answer = input.value.toLowerCase().replaceAll(" ", "")
     if (flag){
         if (answer == puzzle.answer1){
             setTimeout(() => {
                 image.style.backgroundImage = `url(${puzzle.photo3})`
             }, 2000);
             image.style.backgroundImage = `url(${puzzle.photo2})`
+            input.classList.remove("wrongAnswer")
             flag = false
+        }else{
+            input.classList.add("wrongAnswer")
         }
     }else{
         if (answer == puzzle.answer2){
             image.style.backgroundImage = `url(${puzzle.photo4})`
+            input.classList.remove("wrongAnswer")
+            input.classList.add("allRight")
+        }else{
+            input.classList.remove("allRight")
+            input.classList.add("wrongAnswer")
         }
     }
 });
+otherPuzzles.children[0].addEventListener("click", ()=>{
+    let otherPuzzlesChildren = [...otherPuzzles.children]
+    let variants = [...document.querySelectorAll(".hide")]
+    
+    otherPuzzlesChildren.forEach(i => {
+        i.classList.add("hide")
+    })
+    variants.forEach(e =>{
+        e.classList.remove("hide")
+    })
+    let submitBtn = document.querySelector("#submit")
+    mateImage.style.backgroundImage = "url('')"
+    submitBtn.addEventListener("click", ()=>{
+        let selected = document.querySelector("#range").value
+        console.log(selected);
+        
+        if (selected == "200-400"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "400-600"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "600-800"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "800-1000"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1000-1200"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1200-1400"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1400-1600"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1600-1800"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1800-2000"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "2000-2200"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "2200+"){
+            mateImage.style.backgroundImage = "url('')"
+        }
+        
+    })
+})
+otherPuzzles.children[1].addEventListener("click", ()=>{
+    let otherPuzzlesChildren = [...otherPuzzles.children]
+    let variants = [...document.querySelectorAll(".hide")]
+    
+    otherPuzzlesChildren.forEach(i => {
+        i.classList.add("hide")
+    })
+    variants.forEach(e =>{
+        e.classList.remove("hide")
+    })
+    let submitBtn = document.querySelector("#submit")
+    mateImage.style.backgroundImage = "url('')"
+    submitBtn.addEventListener("click", ()=>{
+        let selected = document.querySelector("#range").value
+        console.log(selected);
+        
+        if (selected == "200-400"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "400-600"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "600-800"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "800-1000"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1000-1200"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1200-1400"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1400-1600"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1600-1800"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1800-2000"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "2000-2200"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "2200+"){
+            mateImage.style.backgroundImage = "url('')"
+        }
+        
+    })
+})
+otherPuzzles.children[2].addEventListener("click", ()=>{
+    let otherPuzzlesChildren = [...otherPuzzles.children]
+    let variants = [...document.querySelectorAll(".hide")]
+    
+    otherPuzzlesChildren.forEach(i => {
+        i.classList.add("hide")
+    })
+    variants.forEach(e =>{
+        e.classList.remove("hide")
+    })
+    let submitBtn = document.querySelector("#submit")
+    mateImage.style.backgroundImage = "url('')"
+    submitBtn.addEventListener("click", ()=>{
+        let selected = document.querySelector("#range").value
+        
+        if (selected == "200-400"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "400-600"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "600-800"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "800-1000"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1000-1200"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1200-1400"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1400-1600"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1600-1800"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "1800-2000"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "2000-2200"){
+            mateImage.style.backgroundImage = "url('')"
+        }else if (selected == "2200+"){
+            mateImage.style.backgroundImage = "url('')"
+        }
+        
+    })
+})
