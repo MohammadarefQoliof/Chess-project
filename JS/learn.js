236d31b0dd76d0e3be5caf0cab73054fa982f8a6
Məhəmmədarif Quliyev <mhmmdarifquliyev33@gmail.com>
2025-09-16 22:34:04 +0400

    Chess project

--- JS/learn.js
+++ JS/learn.js
@@ -12,6 +12,7 @@ let moves = document.querySelector(".moves")
 let rules = document.querySelector(".rules")
 let openings = document.querySelector(".openings")
 let tactics = document.querySelector(".tactics")
+let firstPage = document.querySelector(".firstPage")
 console.log(sec1);
 
 li[0].addEventListener("click", ()=>{
@@ -19,6 +20,7 @@ li[0].addEventListener("click", ()=>{
     rules.classList.add("hide")
     openings.classList.add("hide")
     tactics.classList.add("hide")
+    firstPage.classList.add("hide")
     if (window.innerWidth >= 640 && window.innerWidth <= 1024)
     sec1.style.height = "1200px"
 })
@@ -27,16 +29,19 @@ li[1].addEventListener("click", ()=>{
     rules.classList.remove("hide")
     openings.classList.add("hide")
     tactics.classList.add("hide")
+    firstPage.classList.add("hide")
 })
 li[2].addEventListener("click", ()=>{
     moves.classList.add("hide")
     rules.classList.add("hide")
     openings.classList.remove("hide")
     tactics.classList.add("hide")
+    firstPage.classList.add("hide")
 })
 li[3].addEventListener("click", ()=>{
     moves.classList.add("hide")
     rules.classList.add("hide")
     openings.classList.add("hide")
     tactics.classList.remove("hide")
+    firstPage.classList.add("hide")
 })
